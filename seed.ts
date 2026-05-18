7ae42b102be920f9346328a3b273b62ab5e27c03
Amir Bou Ghanem <121335388+AmzBG@users.noreply.github.com>
2026-05-18 13:47:03 +0300

    feat: add Publisher model, update Book relation, and add seeding (#1)

--- seed.ts
+++ seed.ts
@@ -3,23 +3,43 @@ import { faker } from "@faker-js/faker";
 import { prisma } from "./lib/prisma";
 
 async function main() {
-  // Create a new author with a book
-  const author = await prisma.author.create({
-    data: {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      books: {
-        create: {
-          title: faker.book.title(),
+  // Create couple of book publishers
+  const publishers = [];
+  for (let i = 0; i < 5; ++i) {
+    const publisher = await prisma.publisher.create({
+      data: {
+        name: faker.company.name(),
+      },
+    });
+    publishers.push(publisher);
+  }
+  console.log(`Created ${publishers.length} publishers.`);
+  
+  // Create couple of new authors with a book each
+  for (let i = 0; i < 5; ++i) {
+    const author = await prisma.author.create({
+      data: {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        books: {
+          create: [
+            {
+              title: faker.book.title(),
+              publisherId: publishers[i % publishers.length].id,
+            },
+            {
+              title: faker.book.title(),
+              publisherId: publishers[(i + 1) % publishers.length].id,
+            },
+          ],
         },
       },
-    },
-    include: {
-      books: true,
-    },
-  });
-
-  console.log("Created author:", author);
+      include: {
+        books: true,
+      },
+    });
+    console.log(`Created author: ${author.name} with ${author.books.length} books.`);
+  }
 }
 
 main()
